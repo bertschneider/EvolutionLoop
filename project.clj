78f76ef1ee5232721a77ebf56e93bb81be50165a
Norbert Schneider <norbert@BertVM.(none)>
2010-12-19 20:46:22 +0100

    Init.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject de.herrnorbert/EvolutionLoop "1.0.0-SNAPSHOT"
+  :description "A little evolution simulator, like the one in Land of Lisp."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [midje "0.8.1"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
