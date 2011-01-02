73d86fadd1bc5ac20ac2d5d44fd4e5ddc26c855b
Norbert Schneider <norbert@BertVM.(none)>
2011-01-02 18:48:18 +0100

    Changed structs to records.

--- src/de/herrnorbert/EvolutionLoop/gui_repl/gui_repl.clj
+++ src/de/herrnorbert/EvolutionLoop/gui_repl/gui_repl.clj
@@ -3,37 +3,21 @@
 This package provides functions to evaluate a random _world_ and display the steps at the REPL.  
 The code isn't really polished right now so don't be to shocked."
   (:use [de.herrnorbert.EvolutionLoop.core.utils :only [rnd-int-vec]])
-  (:use [de.herrnorbert.EvolutionLoop.core.core :only [update-world]])
+  (:require [de.herrnorbert.EvolutionLoop.core.core :as core])
+  (:import [de.herrnorbert.EvolutionLoop.core.core Animal World])  
   (:use [clojure.pprint]))
-
-
-#_(reduce
- (fn [vals val]
-   (assoc vals val (inc (get vals val 0))))
- {}
- (map #(get-in % [:stats :age]) (:animals @world)))
-
-
-#_(reduce (fn [sums animal]
-          (let [age (get-in animal [:stats :age] 0)
-                sum (inc (get sums age 0))]
-            (assoc sums age sum)))
-        {} (:animals @world))
-
 
 ;; A _world_ with width 100, height 30 and a randomly added _animal_ with random genes.
 (defn gen-world []
-  {:animals
-   [{:location [(rand-int 30) (rand-int 100)]
-     :energy 1000
-     :direction 0
-     :genes (rnd-int-vec 8 10)}]
-   :plants {}
-   :areas [[0 0 100 30] [45 10 10 10]]
-   :options {:width  100
-             :height 30
-             :plant-energy 80
-             :reproduction-energy 100}})
+  (core/World. 
+   [(core/Animal. [(rand-int 30) (rand-int 100)] 1000 0 (rnd-int-vec 8 10) {:age 0})]
+   {}
+   [[0 0 100 30] [45 10 10 10]]
+   {:width  100
+    :height 30
+    :plant-energy 80
+    :reproduction-energy 100}
+   {:age 0}))
 
 ;; This atom holds the current world state and _will be mutated_!
 (def world (atom (gen-world)))
@@ -83,7 +67,7 @@ _Animals_ will be displayed by an `A`, _plants_ by a `*`."
 (defn evolve
   "Evolves the world to its next state."
   []
-  (reset! world (update-world @world)))
+  (reset! world (core/update-world @world)))
 
 (defn evolve-times
   "Evolves the current world `count` times to its next state."
